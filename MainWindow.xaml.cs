aec2bb7655857d55af6700f1a7e5b93b5a19addd
borgeizdev <guilhermecordeiroc25@gmail.com>
2026-03-19 20:42:53 -0300

    Função de Botões e implementação de biblioteca para informações do PC.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,56 @@
 ﻿using System.Windows;
 using System.Windows.Input;
+using Quantyx.Views;
 
 namespace Quantyx {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
             CoreBtn.IsChecked = true;
+            MainContent.Content = new CorePageView();
         }
 
         private void BtnClose_Button(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
+
         private void BtnMinimize_Button(object sender, RoutedEventArgs e) {
             this.WindowState = WindowState.Minimized;
         }
+
         private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
             this.DragMove();
         }
 
+        private void OpenCorePage(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new CorePageView();
+        }
+
+        private void OpenMetricsPage(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new MetricsPageView();
+        }
+
+        private void OpenPerformancePage(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new PerformancePageView();
+        }
+
+        private void OpenProcessesPage(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new ProcessesPageView();
+        }
+
+        private void OpenOverlayPage(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new OverlayPageView();
+        }
+
+        private void OpenSettingsPage(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new SettingsPageView();
+        }
     }
 }
---
+++ Views/CorePageView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using OpenHardwareMonitor.Hardware;
+
+namespace Quantyx.Views
+{
+    public partial class CorePageView : UserControl
+    {
+        private Computer computer;
+        public CorePageView()
+        {
+            InitializeComponent();
+        }
+        public void Monitor()
+        {
+            computer = new Computer() { CPUEnabled = true, GPUEnabled = true, RAMEnabled = true };
+            computer.Open();
+        }
+    }
+}
